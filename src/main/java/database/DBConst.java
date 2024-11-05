4a2082f20306431685632fee26dbecafc14cff3a
Riddhi Savsani <w0862829@myscc.ca>
2024-11-05 12:01:09 -0500

    #34 Added Create table queries to DBConst.java.

--- src/main/java/database/DBConst.java
+++ src/main/java/database/DBConst.java
@@ -66,6 +66,74 @@ public class DBConst {
         public static final String RECURRING_TRANSACTION_COLUMN_TRANSACTION_ID = "transaction_id";
         public static final String RECURRING_TRANSACTION_COLUMN_INTERVAL_DAYS = "interval_days";
 
+        // CREATE TABLES
+
+        // ACCOUNTS TABLE
+        public static final String CREATE_TABLE_ACCOUNTS =
+                "CREATE TABLE " + TABLE_ACCOUNTS + " (" +
+                        ACCOUNTS_COLUMN_ID + " INT NOT NULL AUTO_INCREMENT, " +
+                        ACCOUNTS_COLUMN_ACCOUNT_TYPE + " VARCHAR(50), " +
+                        ACCOUNTS_COLUMN_BALANCE + " DECIMAL(10, 2), " +
+                        ACCOUNTS_COLUMN_INFORMATION + " VARCHAR(255), " +
+                        "PRIMARY KEY (" + ACCOUNTS_COLUMN_ID + ") );";
+
+        // BUDGETS TABLE
+        public static final String CREATE_TABLE_BUDGETS =
+                "CREATE TABLE " + TABLE_BUDGETS + " (" +
+                        BUDGETS_COLUMN_ID + " INT NOT NULL AUTO_INCREMENT, " +
+                        BUDGETS_COLUMN_CATEGORY_ID + " INT, " +
+                        BUDGETS_COLUMN_GOAL_AMOUNT + " DECIMAL(10, 2), " +
+                        BUDGETS_COLUMN_START_DATE + " DATE, " +
+                        BUDGETS_COLUMN_END_DATE + " DATE, " +
+                        "PRIMARY KEY (" + BUDGETS_COLUMN_ID + "), " +
+                        "FOREIGN KEY (" + BUDGETS_COLUMN_CATEGORY_ID + ") REFERENCES " + TABLE_CATEGORIES + "(" + CATEGORIES_COLUMN_ID + ") );";
+
+        // TRANSACTIONS TABLE
+        public static final String CREATE_TABLE_TRANSACTIONS =
+                "CREATE TABLE " + TABLE_TRANSACTIONS + " (" +
+                        TRANSACTIONS_COLUMN_ID + " INT NOT NULL AUTO_INCREMENT, " +
+                        TRANSACTIONS_COLUMN_ACCOUNT_ID + " INT, " +
+                        TRANSACTIONS_COLUMN_AMOUNT + " DECIMAL(10, 2), " +
+                        TRANSACTIONS_COLUMN_TRANSACTION_TYPE_ID + " INT, " +
+                        TRANSACTIONS_COLUMN_TRANSACTION_DATE + " DATE, " +
+                        TRANSACTIONS_COLUMN_DESCRIPTION + " VARCHAR(255), " +
+                        "PRIMARY KEY (" + TRANSACTIONS_COLUMN_ID + "), " +
+                        "FOREIGN KEY (" + TRANSACTIONS_COLUMN_ACCOUNT_ID + ") REFERENCES " + TABLE_ACCOUNTS + "(" + ACCOUNTS_COLUMN_ID + "), " +
+                        "FOREIGN KEY (" + TRANSACTIONS_COLUMN_TRANSACTION_TYPE_ID + ") REFERENCES " + TABLE_TRANSACTION_TYPES + "(" + TRANSACTION_TYPES_COLUMN_ID + ") );";
+
+        // TRANSACTION_TYPE TABLE
+        public static final String CREATE_TABLE_TRANSACTION_TYPES =
+                "CREATE TABLE " + TABLE_TRANSACTION_TYPES + " (" +
+                        TRANSACTION_TYPES_COLUMN_ID + " INT NOT NULL AUTO_INCREMENT, " +
+                        TRANSACTION_TYPES_COLUMN_TYPE + " VARCHAR(50), " +
+                        "PRIMARY KEY (" + TRANSACTION_TYPES_COLUMN_ID + ") );";
+
+        // CATEGORIES TABLE
+        public static final String CREATE_TABLE_CATEGORIES =
+                "CREATE TABLE " + TABLE_CATEGORIES + " (" +
+                        CATEGORIES_COLUMN_ID + " INT NOT NULL AUTO_INCREMENT, " +
+                        CATEGORIES_COLUMN_BUDGET_ID + " INT, " +
+                        CATEGORIES_COLUMN_CATEGORY_TYPE + " VARCHAR(50), " +
+                        "PRIMARY KEY (" + CATEGORIES_COLUMN_ID + "), " +
+                        "FOREIGN KEY (" + CATEGORIES_COLUMN_BUDGET_ID + ") REFERENCES " + TABLE_BUDGETS + "(" + BUDGETS_COLUMN_ID + ") );";
+
+        // TRANSACTION_CATEGORY TABLE
+        public static final String CREATE_TABLE_TRANSACTION_CATEGORY =
+                "CREATE TABLE " + TABLE_TRANSACTION_CATEGORY + " (" +
+                        TRANSACTION_CATEGORY_COLUMN_TRANSACTION_ID + " INT, " +
+                        TRANSACTION_CATEGORY_COLUMN_CATEGORY_ID + " INT, " +
+                        "PRIMARY KEY (" + TRANSACTION_CATEGORY_COLUMN_TRANSACTION_ID + ", " + TRANSACTION_CATEGORY_COLUMN_CATEGORY_ID + "), " +
+                        "FOREIGN KEY (" + TRANSACTION_CATEGORY_COLUMN_TRANSACTION_ID + ") REFERENCES " + TABLE_TRANSACTIONS + "(" + TRANSACTIONS_COLUMN_ID + "), " +
+                        "FOREIGN KEY (" + TRANSACTION_CATEGORY_COLUMN_CATEGORY_ID + ") REFERENCES " + TABLE_CATEGORIES + "(" + CATEGORIES_COLUMN_ID + ") );";
+
+        // RECURRING_TRANSACTION TABLE
+        public static final String CREATE_TABLE_RECURRING_TRANSACTION =
+                "CREATE TABLE " + TABLE_RECURRING_TRANSACTION + " (" +
+                        RECURRING_TRANSACTION_COLUMN_ID + " INT NOT NULL AUTO_INCREMENT, " +
+                        RECURRING_TRANSACTION_COLUMN_TRANSACTION_ID + " INT, " +
+                        RECURRING_TRANSACTION_COLUMN_INTERVAL_DAYS + " INT, " +
+                        "PRIMARY KEY (" + RECURRING_TRANSACTION_COLUMN_ID + "), " +
+                        "FOREIGN KEY (" + RECURRING_TRANSACTION_COLUMN_TRANSACTION_ID + ") REFERENCES " + TABLE_TRANSACTIONS + "(" + TRANSACTIONS_COLUMN_ID + ") );";
 
 
 }
