906468eb32a1dc4a6d63c2d7c5c8e17b637dee15
Matvii Lukavenko <w0865739@myscc.ca>
2024-11-10 23:45:10 -0500

    DB SETUP - commit #1 - ML created constants in the Const class that contain all of the table names and column names.

--- src/main/java/database/Const.java
+++ src/main/java/database/Const.java
@@ -11,6 +11,68 @@ public class Const {
     public static String DB_USER;
     public static String DB_PASS;
 
+    /**
+     * Vars for ACCOUNTS table
+     */
+    public static final String TABLE_ACCOUNTS = "account";
+    public static final String ACCOUNT_COLUMN_ID = "account_id";
+    public static final String ACCOUNT_COLUMN_TYPE = "account_type";
+    public static final String ACCOUNT_COLUMN_balance = "account_balance";
+    public static final String ACCOUNT_COLUMN_INFORMATION = "account_information";
+    /**
+     * Vars for TRANSACTIONS table
+     */
+    public static final String TABLE_TRANSACTIONS = "transactions";
+    public static final String TRANS_COLUMN_ID  = "transaction_id";
+    public static final String TRANS_COLUMN_ACCOUNT_ID = ACCOUNT_COLUMN_ID;
+    public static final String TRANS_COLUMN_AMOUNT = "amount";
+    public static final String TRANS_COLUMN_TYPE = "transaction_type";
+    public static final String TRANS_COLUMN_DATE = "transaction_date";
+    public static final String TRANS_COLUMN_DESCRIPTION = "transaction_description";
+
+    /**
+     * Vars for RECURRING_TRANSACTIONS table
+     */
+    public static final String TABLE_RECURRING_TRANSACTIONS = "recurring_transactions";
+    public static final String REC_TRANS_ID = "recurring_transaction_id";
+    public static final String REC_TRANS_TRANS_ID = TRANS_COLUMN_ID;
+    public static final String REC_TRANS_INTERVAL = "interval";
+
+    /**
+     * Vars for CATEGORIES table
+     */
+    public static final String TABLE_CATEGORIES = "categories";
+    public static final String CATEGORIES_COLUMN_ID = "category_id";
+    //CATEGORIES_COLUMN_BUDGET_ID was here
+    public static final String CATEGORIES_COLUMN_NAME = "category_name";
+    /**
+     * Vars for BUDGETS table
+     * Category Id - created in categories
+     * Budget id - created in budgets
+     */
+    public static final String TABLE_BUDGETS = "budgets";
+    public static final String BUDGETS_COLUMN_ID = "budget_id";
+    public static final String BUDGETS_COLUMN_CATEGORY_ID = CATEGORIES_COLUMN_ID;
+    public static final String BUDGETS_COLUMN_GOAL_AMOUNT = "goal_amount";
+    public static final String BUDGETS_COLUMN_START_DATE = "start_date";
+    public static final String BUDGETS_COLUMN_END_DATE = "end_date";
+
+    /**
+     * This needs to be here if we're to get budget id from budgets
+     */
+    public static final String CATEGORIES_COLUMN_BUDGET_ID = BUDGETS_COLUMN_ID;
+
+
+
+    /**
+     * Vars for TRANSACTION_CATEGORY table
+     */
+    public static final String TABLE_TRANSACTION_CATEGORY = "transaction_category";
+    public static final String TRANS_CATEGORY_ID = TRANS_COLUMN_ID;
+    public static final String TRANS_CATEGORY_CATEGORY_ID = CATEGORIES_COLUMN_ID;
+
+
+
     public Const() {
         try (Scanner scanner = new Scanner(new File(dbCredentialsPath))){
             DB_HOST = scanner.nextLine();
