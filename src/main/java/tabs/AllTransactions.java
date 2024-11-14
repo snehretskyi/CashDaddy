ea095e4fb2498eaac9b2bfe10d79ce0a31ff4e58
Riddhi Savsani <w0862829@myscc.ca>
2024-11-14 14:55:45 -0500

    Added columns to tableView

--- src/main/java/tabs/AllTransactions.java
+++ src/main/java/tabs/AllTransactions.java
@@ -27,6 +27,65 @@ public class AllTransactions extends Tab {
 
         tableView = new TableView();
 
+
+        /**
+         * Column1 for displaying account name
+         */
+
+        TableColumn<DisplayTransaction, String> column1 =
+                new TableColumn<>("Account Name");
+
+        column1.setCellValueFactory(
+                e-> new SimpleStringProperty(e.getValue().getAccount_name()));
+
+        /**
+         * Column2 for displaying transaction amount
+         */
+
+        TableColumn<DisplayTransaction, String> column2 =
+                new TableColumn<>("Transaction Amount");
+
+        column2.setCellValueFactory(
+                e-> new SimpleStringProperty(e.getValue().getTransaction_amount()));
+
+        /**
+         * Column3 for displaying transaction type
+         */
+
+        TableColumn<DisplayTransaction, String> column3 =
+                new TableColumn<>("Transaction Type");
+
+        column3.setCellValueFactory(
+                e-> new SimpleStringProperty(e.getValue().getTransaction_type()));
+
+        /**
+         * Column4 for displaying transaction date
+         */
+
+        TableColumn<DisplayTransaction, String> column4 =
+                new TableColumn<>("Transction Date");
+
+        column4.setCellValueFactory(
+                e-> new SimpleStringProperty(e.getValue().getTransaction_date()));
+
+        /**
+         * Column5 for displaying transaction description
+         */
+
+        TableColumn<DisplayTransaction, String> column5 =
+                new TableColumn<>("Transction Description");
+
+        column5.setCellValueFactory(
+                e-> new SimpleStringProperty(e.getValue().getTransaction_description()));
+
+        /**
+         * added all columns to tableView
+         */
+
+        tableView.getColumns().addAll(column1, column2, column3, column4);
+
+
+
     }
 
 
