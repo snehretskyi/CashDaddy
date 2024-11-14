8bcf9d0d090038778c84fe5631fe4d5a3d69077e
Riddhi Savsani <w0862829@myscc.ca>
2024-11-14 13:31:17 -0500

    Implemented AllTransactions class as a singleton

--- src/main/java/tabs/AllTransactions.java
+++ src/main/java/tabs/AllTransactions.java
@@ -1,9 +1,31 @@
 package tabs;
 
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
 
 public class AllTransactions extends Tab {
-    public AllTransactions(){
+
+    /**
+     * Singleton class AllTransactions .
+     */
+
+    private static AllTransactions instance;
+    public TableView tableView;
+
+    private AllTransactions(){
+
         this.setText("Transactions");
+
+
+
     }
+
+
+    public static AllTransactions getInstance(){
+        if(instance == null){
+            instance = new AllTransactions();
+        }
+        return instance;
+    }
+
 }
